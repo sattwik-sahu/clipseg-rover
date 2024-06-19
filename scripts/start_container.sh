f4ab893d83486652282b3f6c3cd9d230934b4482
jnash10 <agamdeep20@iiserb.ac.in>
2024-06-19 13:05:23 +0000

    chores:removed --rm from start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Start the ROS 2 development container with GUI support and X11 forwarding
-docker run -d -it --rm --privileged \
+docker run -d -it --privileged \
   --net=host \
   --ipc=host \
   --gpus="all" \
   --runtime="nvidia" \
   --env="DISPLAY" \
-  -v $PWD:/home/sattwik/vislang-map:rw \
+  -v $PWD:/home/sattwik/clipseg-rover:rw \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
   --volume="${XAUTHORITY}:/root/.Xauthority:rw" \
   -e "TERM=xterm-256color" \
