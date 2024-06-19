28142ea9b43bab64122eb8fc310df925dbd63c7d
karthiknambiar29 <karthiknambiar4u@gmail.com>
2024-06-19 17:46:30 +0530

    a

--- run/run_bag.py
+++ run/run_bag.py
@@ -1,43 +1,46 @@
-import os
+import argparse
 import rosbag
 import cv2
 from cv_bridge import CvBridge
 import matplotlib.pyplot as plt
-from PIL import Image
-from torchvision import transforms
 from models.clipseg_gpu import CLIPDensePredT
 import torch
 import numpy as np
 from run_utils import load_model, get_transform, get_square_image
 
+def main(bag_file_path, prompts, topic):
+    bridge = CvBridge()
+    model = load_model()
+    transform = get_transform()
+    i = 0
 
-bag_file_path = 'example_filtered.bag'
+    with rosbag.Bag(bag_file_path, 'r') as bag:
+        for i, (topic_msg, msg, t) in enumerate(bag.read_messages()):
+            if topic_msg == topic:
+                PREDS = []
+                cv_image = bridge.imgmsg_to_cv2(msg, "rgb8")
+                cv_image = get_square_image(cv_image)
+                img = transform(cv_image).unsqueeze(0)
 
-bridge = CvBridge()
-prompts = ['navigable pathway', 'dirt road']
-model = load_model()
-transform = get_transform()
-i = 0
+                img = img.half()
+                img = img.to('cuda')
+                with torch.no_grad():
+                    preds = model(img.repeat(len(prompts), 1, 1, 1), prompts)[0]
 
-with rosbag.Bag(bag_file_path, 'r') as bag:
-    for topic, msg, t in bag.read_messages():
-        if topic == '/nerian/right/image_raw':
-            PREDS = []
-            cv_image = bridge.imgmsg_to_cv2(msg, "rgb8")
-            cv_image = get_square_image(cv_image)
-            img = transform(cv_image).unsqueeze(0)
-                
-            img = img.half()
-            img = img.to('cuda')
-            with torch.no_grad():
-                preds = model(img.repeat(len(prompts),1,1,1), prompts)[0]
-                
-            _, ax = plt.subplots(1, len(prompts)+1, figsize=(15, 4))
-            [a.axis('off') for a in ax.flatten()]
-            ax[0].imshow(cv_image)
-            [ax[i+1].imshow(cv2.resize(np.float32(np.transpose(torch.sigmoid(preds[i][0]).cpu().unsqueeze(0).numpy(), (1, 2, 0))), (592, 592), interpolation = cv2.INTER_CUBIC)) for i in range(len(prompts))]
-            [ax[i+1].text(0, -15, prompts[i]) for i in range(len(prompts))]
-            plt.savefig(f'images/{str(i).zfill(5)}.png')
-            plt.close()
-            i+= 1
+                _, ax = plt.subplots(1, len(prompts) + 1, figsize=(15, 4))
+                [a.axis('off') for a in ax.flatten()]
+                ax[0].imshow(cv_image)
+                [ax[i + 1].imshow(cv2.resize(np.float32(np.transpose(torch.sigmoid(preds[i][0]).cpu().unsqueeze(0).numpy(), (1, 2, 0))), (592, 592), interpolation=cv2.INTER_CUBIC)) for i in range(len(prompts))]
+                [ax[i + 1].text(0, -15, prompts[i]) for i in range(len(prompts))]
+                plt.savefig(f'images/{str(i).zfill(5)}.png')
+                plt.close()
+                i += 1
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process a ROS bag file and overlay image masks.')
+    parser.add_argument('--bag_file_path', type=str, default='example_filtered.bag', help='Path to the ROS bag file.')
+    parser.add_argument('--prompts', nargs='+', default=['navigable pathway', 'dirt road'], help='List of prompts for the model.')
+    parser.add_argument('--topic', type=str, default='/nerian/right/image_raw', help='The topic to filter from the ROS bag.')
+
+    args = parser.parse_args()
+    main(args.bag_file_path, args.prompts, args.topic)
--- run/run_folder.py
+++ run/run_folder.py
@@ -1,3 +1,4 @@
+import argparse
 import cv2
 import os
 from natsort import natsorted
@@ -6,37 +7,46 @@ import matplotlib.pyplot as plt
 import torch
 import numpy as np
 
-image_folder = '/workspace/RUGD_frames/creek'
+def process_images(image_folder, prompts, output_folder):
+    model = load_model()
+    transform = get_transform()
 
-model = load_model()
-transform = get_transform()
-prompts = ['navigable pathway', 'stone pathway']
+    image_files = [f for f in os.listdir(image_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
+    image_files = natsorted(image_files)
 
-image_files = [f for f in os.listdir(image_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
-image_files = natsorted(image_files)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
+    for image_file in image_files:
+        image_path = os.path.join(image_folder, image_file)
+        frame = cv2.imread(image_path)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = get_square_image(frame)
 
-for image_file in image_files:
-    image_path = os.path.join(image_folder, image_file)
-    frame = cv2.imread(image_path)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = get_square_image(frame)
+        img = transform(frame).unsqueeze(0)
+        img = img.half()
+        img = img.to('cuda')
+        with torch.no_grad():
+            preds = model(img.repeat(len(prompts), 1, 1, 1), prompts)[0]
+        preds = [torch.sigmoid(preds[i][0]).cpu().unsqueeze(0).numpy() for i in range(len(prompts))]
+        
+        _, ax = plt.subplots(1, len(prompts) + 1, figsize=(15, 4))
+        [a.axis('off') for a in ax.flatten()]
+        ax[0].imshow(frame)
+        [ax[i + 1].imshow(frame) for i in range(len(prompts))]
+        [ax[i + 1].imshow(np.float32(np.transpose(preds[i], (1, 2, 0))), alpha=0.5) for i in range(len(prompts))]
+        [ax[i + 1].text(0, -15, prompts[i]) for i in range(len(prompts))]
+        
+        output_path = os.path.join(output_folder, image_file)
+        plt.savefig(output_path)
+        print(f'Saved: {output_path}')
+        plt.close()
 
-    img = transform(frame).unsqueeze(0)
-    img = img.half()
-    img = img.to('cuda')
-    with torch.no_grad():
-        preds = model(img.repeat(len(prompts),1,1,1), prompts)[0]
-    preds = [torch.sigmoid(preds[i][0]).cpu().unsqueeze(0).numpy() for i in range(len(prompts))]
-    _, ax = plt.subplots(1, len(prompts)+1, figsize=(15, 4))
-    [a.axis('off') for a in ax.flatten()]
-    ax[0].imshow(frame)
-    [ax[i+1].imshow(frame) for i in range(len(prompts))]
-    [ax[i+1].imshow(np.float32(np.transpose(preds[i], (1, 2, 0))), alpha=0.5) for i in range(len(prompts))]
-    [ax[i+1].text(0, -15, prompts[i]) for i in range(len(prompts))]
-    plt.savefig(f'/workspace/rugd_predictions/creek/{image_file}')
-    print(image_file)
-    plt.close()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process images and overlay predictions.')
+    parser.add_argument('--image_folder', type=str, default='/workspace/RUGD_frames/creek', help='Folder containing the images.')
+    parser.add_argument('--prompts', nargs='+', default=['navigable pathway', 'stone pathway'], help='List of prompts for the model.')
+    parser.add_argument('--output_folder', type=str, default='/workspace/rugd_predictions/creek', help='Folder to save the output images.')
 
-
-    
+    args = parser.parse_args()
+    process_images(args.image_folder, args.prompts, args.output_folder)
